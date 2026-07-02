ca4ca731aee6bac896ed4ca364c4f215ff46eebb
modernhuman863-oss <modernhuman863@gmail.com>
2026-07-02 14:47:53 +1000

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+let person="Steve Irwin";
+
+function changeTitle ( ) {let title=
+document.getElementById("title"); textContent = person;
+if (title.textContent=="Ledgend") {
+title.textContent=person;
+}else
+{
+title.textContent="Ledgend";
+}
+}
